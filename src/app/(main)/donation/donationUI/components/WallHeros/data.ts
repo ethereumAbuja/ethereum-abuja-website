fec03f6f0288ed848643f4ead6b49258dba486fd
Victoria <adedayovicky123@gmail.com>
2024-01-18 07:04:13 +0100

    Sponsor banner
    install react-marquee
    
    add data.ts file with the names of sponsors
    
    add background and text colors to all boxes with names of sponsors in them

---
+++ src/app/(main)/donation/donationUI/components/WallHeros/data.ts
@@ -0,0 +1,24 @@
+const sponsorNames: string[] = [
+  "John",
+  "Jane",
+  "Michael",
+  "Emily",
+  "Christopher",
+  "Olivia",
+  "William",
+  "Sophia",
+  "Matthew",
+  "Emma",
+  "Daniel",
+  "Ava",
+  "Ethan",
+  "Grace",
+  "David",
+  "Lily",
+  "Christopher",
+  "Mia",
+  "Alexander",
+  "Ella",
+];
+
+export default sponsorNames;
--- src/app/(main)/donation/donationUI/components/WallHeros/index.tsx
+++ src/app/(main)/donation/donationUI/components/WallHeros/index.tsx
@@ -1,11 +1,52 @@
 import React from "react";
 import { Box, Text } from "@chakra-ui/react";
+import Marquee from "react-fast-marquee";
+import sponsorNames from "./data";
 
 const WallHeros = () => {
+  const backgroundColors = [
+    "#FFFFFF",
+    "#000000",
+    "#0400DE",
+    "#FF5733",
+    "#66FF66",
+  ];
+  const textColors = ["#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000"];
+
   return (
-    <Box pt="5%">
-      <Text textAlign="center">WALL OF HEROES (Sponsors) 🧑‍🚒</Text>
-    </Box>
+    <>
+      <Box pt="5%">
+        <Text textAlign="center">WALL OF HEROES (Sponsors) 🧑‍🚒</Text>
+      </Box>
+      <Marquee pauseOnHover direction="right" speed={100}>
+        {sponsorNames.map((name, index) => (
+          <Box
+            key={index}
+            px="20px"
+            py="15px"
+            m="5px"
+            bg={backgroundColors[index % backgroundColors.length]}
+            color={textColors[index % textColors.length]}
+          >
+            <Text>{name}</Text>
+          </Box>
+        ))}
+      </Marquee>
+      <Marquee pauseOnHover direction="left" speed={100}>
+        {sponsorNames.map((name, index) => (
+          <Box
+            key={index}
+            px="20px"
+            py="15px"
+            mx="5px"
+            bg={backgroundColors[index % backgroundColors.length]}
+            color={textColors[index % textColors.length]}
+          >
+            <Text>{name}</Text>
+          </Box>
+        ))}
+      </Marquee>
+    </>
   );
 };
 
